62740cb36872133a07117b6bbb829d6e3982bd8e
Michael Lobov <michael.lobov13@gmail.com>
2021-04-24 19:18:50 +0300

    9. toString [#515141]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,8 +2,7 @@ package ru.job4j.tracker;
 
 public class StartUI {
 	public static void main(String[] args) {
-		Tracker tracker = new Tracker();
-		tracker.add(new Item());
-		System.out.println(tracker.findById(1));
+		Item item = new Item();
+		System.out.println(item);
 	}
 }
