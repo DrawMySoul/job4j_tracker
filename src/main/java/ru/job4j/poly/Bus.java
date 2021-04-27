53a142306e3fbfcf0bee2402876d3741f5f110dc
Michael Lobov <michael.lobov13@gmail.com>
2021-04-27 14:25:52 +0300

    3. Интерфейс Input [#515058] remarks correction

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -3,16 +3,16 @@ package ru.job4j.poly;
 public class Bus implements Transport {
 	@Override
 	public void drive() {
-
+		System.out.println(getClass().getSimpleName() + " едет");
 	}
 
 	@Override
 	public void passengers(int passengers) {
-
+		System.out.println(passengers + " пассажиров село в транспорт");
 	}
 
 	@Override
 	public double gasStation(double fuel) {
-		return 0;
+		return fuel * 45.5;
 	}
 }
