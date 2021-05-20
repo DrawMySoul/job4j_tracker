b5731cfd663b8049d412da08a253195c84c3ed8c
DrawMySoul <itismineeee@gmail.com>
2021-05-20 17:34:20 +0300

    2. Отсортировать департаменты [#515025]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -8,12 +8,6 @@ public class DepDescComp implements Comparator<String> {
         String startOfFirst = first.split("/")[0];
         String startOfSecond = second.split("/")[0];
         int result = startOfFirst.compareTo(startOfSecond);
-        if (result > 0) {
-            return -1;
-        } else if (result < 0) {
-            return 1;
-        } else {
-            return first.compareTo(second);
-        }
+        return result == 0 ? first.compareTo(second) : result * -1;
     }
 }
