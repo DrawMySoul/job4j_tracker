3bd13b28e398895ba5fb30169757d8f631854105
DrawMySoul <itismineeee@gmail.com>
2021-05-25 16:37:21 +0300

    2. Список адресов [#110062]

---
+++ src/test/java/ru/job4j/stream/ProfilesTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.stream;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class ProfilesTest {
+    @Test
+    public void checkProfiles() {
+        List<Profile> profiles = List.of(
+                new Profile(new Address("St. Petersburg", "Lenin Street", 19, 17)),
+                new Profile(new Address("Moscow", "Stalin street", 19, 37))
+        );
+        List<Address> expected = List.of(
+                new Address("St. Petersburg", "Lenin Street", 19, 17),
+                new Address("Moscow", "Stalin street", 19, 37)
+        );
+        List<Address> result = Profiles.collect(profiles);
+        assertThat(result, is(expected));
+    }
+
+}
